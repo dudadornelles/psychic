53cb1fb0cdeff86692d8c0c14a59b99ca7e3c021
Duda Dornelles <ddornell@thoughtworks.com>
2015-08-12 11:05:53 -0300

    Fixing .F.F printing to do once for test

--- psychic/csource/psychic.c
+++ psychic/csource/psychic.c
@@ -5,30 +5,21 @@
 
 int pc_total_assertions;
 
-void pc_succeed() {
-        printf(".");
-}
-
 void pc_fail() {
         pc_current_test_failed = 1;
-        printf("F");
 }
 
 void assert_true(int r) {
         pc_total_assertions++;
-        if (r) {
-                pc_succeed();
-        } else {
-                pc_fail();
-        }
+        if (!r) {
+            pc_fail();
+        } 
 }
 
 void assert_equals_int(int a, int b) {
         pc_total_assertions++;
-        if (a == b) {
-                pc_succeed();
-        } else {
-                pc_fail();
+        if (a != b) {
+            pc_fail();
         }
 }
 
