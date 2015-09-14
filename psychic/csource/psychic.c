5c8a8a77da1b6be3159c74122edbe20cfbf9c7f9
Duda Dornelles <dudassdornelles@gmail.com>
2015-09-14 10:21:33 -0700

    adding line numbers to error report

--- psychic/csource/psychic.c
+++ psychic/csource/psychic.c
@@ -5,6 +5,7 @@
 #include "psychic.h"
 #include "psychic_share.h"
 
+
 int pc_total_assertions;
 int pc_current_test_failed;
 FILE* pc_error_messages;
@@ -14,137 +15,132 @@ void pc_fail() {
     pc_current_test_failed = 1;
 }
 
-void assert(int b)
-{
-    assert_true(b);
-}
-
-void assert_true(int b) 
+void assert_true_r(int b, int line_number) 
 {
     pc_total_assertions++;
     if (!b) {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tAssertion Failure.");
         pc_current_test_failed = 1;
     } 
 }
 
-void assert_false(int b) 
+void assert_false_r(int b, int line_number) 
 {
     pc_total_assertions++;
     if (b) {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tAssertion Failure.");
         pc_fail();
     } 
 }
 
-void assert_null(void *p)
+void assert_null_r(void *p, int line_number)
 {
     pc_total_assertions++;
     if (p != 0)
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tAssertion Failure.");
         pc_fail();
     }
 }
 
-void assert_not_null(void *p)
+void assert_not_null_r(void *p, int line_number)
 {
     pc_total_assertions++;
     if (p == 0)
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tAssertion Failure.");
         pc_fail();
     }
 }
 
-void assert_equals_str(char *a, char *b) 
+void assert_equals_str_r(char *a, char *b, int line_number) 
 {
     pc_total_assertions++;
     if (strcmp(a, b) != 0) 
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %s to equals %s", a, b);
         pc_fail();
     }
 }
 
-void assert_not_equals_str(char *a, char *b)
+void assert_not_equals_str_r(char *a, char *b, int line_number)
 {
     pc_total_assertions++;
     if (strcmp(a, b) == 0) 
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %s to not equals %s", a, b);
         pc_fail();
     }
 }
 
-void assert_equals_int(int a, int b) 
+void assert_equals_int_r(int a, int b, int line_number) 
 {
     pc_total_assertions++;
     if (a != b) 
     {
         pc_fail();
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %i to equals %i", a, b);
     }
 }
 
-void assert_not_equals_int(int a, int b)
+void assert_not_equals_int_r(int a, int b, int line_number)
 {
     pc_total_assertions++;
     if (a == b) 
     {
         pc_fail();
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %i to not equals %i", a, b);
     }
 }
 
-void assert_equals_float(float a, float b)
+void assert_equals_float_r(float a, float b, int line_number)
 {
     pc_total_assertions++;
     if (a != b) 
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %f to equals %f", a, b);
         pc_fail();
     }
 }
 
-void assert_not_equals_float(float a, float b)
+void assert_not_equals_float_r(float a, float b, int line_number)
 {
     pc_total_assertions++;
     if (a != b) 
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %f to not equals %f", a, b);
         pc_fail();
     }
 }
 
-void assert_equals_double(double a, double b)
+void assert_equals_double_r(double a, double b, int line_number)
 {
     pc_total_assertions++;
     if (a != b) 
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %f to equals %f", a, b);
         pc_fail();
     }
 
 }
 
-void assert_not_equals_double(double a, double b)
+void assert_not_equals_double_r(double a, double b, int line_number)
 {
     pc_total_assertions++;
     if (a == b) 
     {
-        fprintf(pc_error_messages, "\nError in test %s :", pc_current_test_name);
+        fprintf(pc_error_messages, "\nError in test %s:%d", pc_current_test_name, line_number);
         fprintf(pc_error_messages, "\n\tExpected %f to not equals %f", a, b);
         pc_fail();
     }
--- psychic/csource/psychic.h
+++ psychic/csource/psychic.h
@@ -2,21 +2,48 @@
 #define PSYCHIC_H
 
 void assert(int r);
-void assert_true(int r);
-void assert_false(int r);
 
-void assert_null(void *p);
-void assert_equals_int(int a, int b);
-void assert_equals_str(char *a, char *b);
-void assert_equals_char(char a, char b);
-void assert_equals_float(float a, float b);
-void assert_equals_double(double a, double b);
+void assert_true_r(int a, int line_number);
+#define assert_true(a)  (assert_true_r(a, __LINE__))
+#define assert(a)  (assert_true_r(a, __LINE__))
 
-void assert_not_null(void *p);
-void assert_not_equals_int(int a, int b);
-void assert_not_equals_str(char *a, char *b);
-void assert_not_equals_char(char a, char b);
-void assert_not_equals_float(float a, float b);
-void assert_not_equals_double(double a, double b);
+void assert_false_r(int r, int line_number);
+#define assert_false(a)  (assert_false_r(a, __LINE__))
+
+void assert_null_r(void *p, int line_number);
+#define assert_null(a)  (assert_null_r(a, __LINE__))
+
+void assert_equals_int_r(int a, int b, int line_number);
+#define assert_equals_int(a, b)  (assert_equals_int_r(a, b, __LINE__))
+
+void assert_equals_str_r(char *a, char *b, int line_number);
+#define assert_equals_str(a, b)  (assert_equals_str_r(a, b, __LINE__))
+
+void assert_equals_char_r(char a, char b);
+#define assert_equals_char(a, b) (assert_equals_char_r(a, b, __LINE__))
+
+void assert_equals_float_r(float a, float b, int line_number);
+#define assert_equals_float(a, b) (assert_equals_float_r(a, b, __LINE__))
+
+void assert_equals_double_r(double a, double b, int line_number);
+#define assert_equals_double(a, b) (assert_equals_double_r(a, b, __LINE__))
+
+void assert_not_null_r(void *p, int line_number);
+#define assert_not_null(p) (assert_not_null_r(p, __LINE__))
+
+void assert_not_equals_int_r(int a, int b, int line_number);
+#define assert_not_equals_int(a, b) (assert_not_equals_int_r(a, b, __LINE__))
+
+void assert_not_equals_str_r(char *a, char *b, int line_number);
+#define assert_not_equals_str(a, b)  (assert_not_equals_str_r(a, b, __LINE__))
+
+void assert_not_equals_char_r(char a, char b, int line_number);
+#define assert_not_equals_char(a, b)  (assert_not_equals_char_r(a, b, __LINE__))
+
+void assert_not_equals_float_r(float a, float b, int line_number);
+#define assert_not_equals_float(a, b)  (assert_not_equals_float_r(a, b, __LINE__))
+
+void assert_not_equals_double_r(double a, double b, int line_number);
+#define assert_not_equals_double(a, b)  (assert_not_equals_double_r(a, b, __LINE__))
 
 #endif
